67c142177507c92fd2d8dc198de3e12d5a635755
Urko Lopez <urkolopezlopez@gmail.com>
2024-07-30 18:26:13 +0200

    Añadido orden y campos numéricos

--- sql/tipo_bomba.sql
+++ sql/tipo_bomba.sql
@@ -1,16 +1,17 @@
 alter table t_parte_linea
+    add column orden_bomba int4,
     add column tipo_bomba varchar,
     add column marca_bomba varchar,
     add column modelo_bomba varchar,
     add column fecha_bomba date,
     add column motor_bomba varchar,
-    add column voltaje_bomba varchar,
-    add column rpm_bomba varchar,
+    add column voltaje_bomba float8,
+    add column rpm_bomba float8,
     add column manometro_bomba varchar,
     add column esfera_bomba varchar,
     add column valvulas_bomba varchar,
     add column saltos_bomba varchar,
     add column fusibles_bomba varchar,
     add column alarma_bomba varchar,
-    add column caudalimetro_bomba varchar,
-    add column presion_bomba varchar;
+    add column caudalimetro_bomba float8,
+    add column presion_bomba float8;
